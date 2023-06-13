22c75c5165eb67d334b917b91355b8aca046f7f5
Illia Bidiuk <Core778@gmail.com>
2023-06-13 21:49:33 +0300

    first commit

--- src/components/pages/Cast.jsx
+++ src/components/pages/Cast.jsx
@@ -1,12 +1,13 @@
 import React, { useState, useEffect  } from 'react';
 import { useParams } from 'react-router-dom';
 
+// ...импорт React и других необходимых модулей
+
 const Cast = () => {
   const { movieId } = useParams();
   const [data, setData] = useState(null);
 
   useEffect(() => {
-
     const options = { method: 'GET', headers: { accept: 'application/json' } };
     const API_KEY = '619e0771d50f6f0cacad940951db2b61';
     const url = `https://api.themoviedb.org/3/movie/${movieId}/credits?api_key=${API_KEY}&language=en-US`;
@@ -18,30 +19,31 @@ const Cast = () => {
         }
         throw new Error('Something went wrong');
       })
-      .then(({ results }) => {
-        setData(results);
+      .then((response) => {
+        setData(response);
       })
       .catch((error) => {
         console.error(error);
       });
   }, [movieId]);
-  const cast = data.cast;
 
   return (
     <div>
       <h3>Cast:</h3>
+    
       {data && (
         <ul>
-          {cast.map((item) => {
-            <li>
-              <img src={`https://image.tmdb.org/t/p/w200${item.poster_path}`} alt={item.title} />
+          {data.cast.map((item) => (
+            <li key={item.id}>
+              <img src={`https://image.tmdb.org/t/p/w200${item.profile_path}`} alt={item.original_name} />
               <p>Actor: {item.original_name}</p>
-              <p>Character {item.character}</p>
+              <p>Character: {item.character}</p>
             </li>
-          })}
+          ))}
         </ul>
       )}
     </div>
   );
-}
-export default Cast
+};
+
+export default Cast;
